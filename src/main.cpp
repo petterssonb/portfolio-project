ab70fb94a991b8c50a735bea753f93848c0aa8f2
Erik Pettersson <erikpettersson02@gmail.com>
2024-11-07 11:41:39 +0100

    Configure PlatformIO project and initial source files

---
+++ src/main.cpp
@@ -0,0 +1,65 @@
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLE2902.h>
+#include "DHT.h"
+#include "config.h"
+
+#define DHTPIN 27
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+BLECharacteristic *pCharacteristic;
+bool deviceConnected = false;
+
+class MyCallbacks : public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+        deviceConnected = true;
+    }
+
+    void onDisconnect(BLEServer* pServer) {
+        deviceConnected = false;
+    }
+};
+
+void setup() {
+    Serial.begin(115200);
+    dht.begin();
+
+    BLEDevice::init("ESP32_Sensor");
+    BLEServer *pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyCallbacks());
+
+    BLEService *pService = pServer->createService(SERVICE_UUID);
+    pCharacteristic = pService->createCharacteristic(
+                        CHARACTERISTIC_UUID,
+                        BLECharacteristic::PROPERTY_NOTIFY
+                      );
+    pCharacteristic->addDescriptor(new BLE2902());
+
+    pService->start();
+    BLEAdvertising *pAdvertising = pServer->getAdvertising();
+    pAdvertising->addServiceUUID(SERVICE_UUID);
+    pAdvertising->start();
+    Serial.println("Bluetooth initialized and advertising.");
+}
+
+void loop() {
+    float temperature = dht.readTemperature();
+    float humidity = dht.readHumidity();
+
+    if (isnan(temperature) || isnan(humidity)) {
+        Serial.println("Failed to read from DHT sensor!");
+        return;
+    }
+
+    String jsonData = "{\"temperature\": " + String(temperature) + ", \"humidity\": " + String(humidity) + "}";
+    if (deviceConnected) {
+        pCharacteristic->setValue(jsonData.c_str());
+        pCharacteristic->notify();
+        Serial.println("Data sent via BLE: " + jsonData);
+    } else {
+        Serial.println("BLE not connected.");
+    }
+    delay(60000);
+}
